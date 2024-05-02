018ff8ab4dd8757d4d8daf7e016990d0fd784ddd
Obloqulov Shoyim <92427513+shoyimobloqulov@users.noreply.github.com>
2024-05-02 14:51:25 +0500

    Update main.py
    Key in Text encoder function

--- Yuliy Sezar/main.py
+++ Yuliy Sezar/main.py
@@ -1,11 +1,33 @@
-txt = "KOMPYUTER"
 key = "DIPLOMAT"
-k = 5
+k = 3
 
-l = ['' for x in range(26)]
+n = "SALOM"
 
-l[k-1:len(key)] = key
-for i in range(k,26):
-    if i >= len(key):
-        l[i] = f()
-print(l)
+
+arr = ['' for i in range(26)]
+sub_arr = [chr(65 + i) for i in range(26)]
+arr[k:len(key)]=key
+"""
+SALOM
+
+"""
+
+i = 0
+nw = ""
+while(i < 26):
+    if not(chr(i + 65) in key):
+        nw += chr(i + 65)
+    i += 1
+arr[len(key) + k:] = nw[:26-k - len(key)]
+arr[:k] = nw[26-k - len(key):]
+
+d = dict()
+
+for i in range(26):
+    d.update({sub_arr[i]:arr[i]})
+
+code_key = ""
+
+for i in n:
+    code_key += d[i]
+print(code_key)
